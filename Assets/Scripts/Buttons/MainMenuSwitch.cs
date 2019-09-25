5da6e13404de6d29b61690e67498ecdc385f62fe
Neo_Kesha <neokesha@yandex.ru>
2019-09-25 17:57:50 +0700

    Main menu

---
+++ Assets/Scripts/Buttons/MainMenuSwitch.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuSwitch : MonoBehaviour
+{
+    public GameObject From = null;
+    public GameObject To = null;
+    public void OnClick() {
+        if (To && From) {
+            To.SetActive(true);
+            From.SetActive(false);
+        }
+    }
+}
